4d0f02ef704f4ce4a3e6688cd97e1c7656643a18
choi232 <148494133+choi232@users.noreply.github.com>
2024-10-30 09:35:01 -0400

    Update Board.java
    added comments manually

--- Board.java
+++ Board.java
@@ -1,1 +1,12 @@
 //Board File
+
+//Board File
+
+//how to build the board game box
+
+//2 boards letters down the side and numbers across the top (10x10 board)
+
+//5 different ship types
+//Carrier has 5 pegs
+
+//2 colored set of pets

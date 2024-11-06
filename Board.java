72db7dc4ce00560292a234a10c9c1561a89c5d2b
choimik <161179509+choimik@users.noreply.github.com>
2024-11-05 23:57:16 -0500

    tested functionality of checkShip function as well as some comments

--- Board.java
+++ Board.java
@@ -17,7 +17,7 @@ public class Board{
 	
 	//char[][] board = new char[10][10];
 	
-	char[][] board = {{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'}};
+	char[][] board = {{'0','S','0','0','S','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'}};
 	
 	/*
 	for(int r = 0; r < 10; r++){
@@ -36,7 +36,7 @@ public class Board{
 	 
 	 */
 	public boolean checkShip(int row, int col){
-		if(board[row][col] == 's'){
+		if(board[row][col] == 'S'){
 			return true;
 		}
 		return false;
@@ -49,12 +49,13 @@ public class Board{
 	public void printBoard(){ //if you name the method toString it will not allow you to run it since it would be overriding toString from String library so I called it printBoard
 		int letter = 65;
 		System.out.println("  0 1 2 3 4 5 6 7 8 9"); //header for columns also used 0-9 instead of 0-10 otherwise there will be an uneven spacing
-		for(int r = 0; r < 10; r++, letter++){
-			if(letter < 74) System.out.printf("%c ", letter);
+		//iterate through 2d array and print
+		for(int r = 0; r < 10; r++, letter++){ //increment both r and letter at same time in for loop
+			System.out.printf("%c ", letter);//print out side header of letters
 			for(int c = 0; c < 10; c++){
-				System.out.print(board[r][c] + " ");
+				System.out.print(board[r][c] + " ");//access 2d array and print individual value
 			}
-			System.out.println();
+			System.out.println(); //print \n to create line break between each row
 		}
 	}
 }
--- Tester.java
+++ Tester.java
@@ -4,6 +4,7 @@ public class Tester{
 	public static void main(String[] args){
 		Board b = new Board();
 		b.printBoard();
+		System.out.println(b.checkShip(0,1));
 	}
 
 }

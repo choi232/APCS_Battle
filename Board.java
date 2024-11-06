0149d718865a214ab234c9a4145e8300c5e42c28
choimik <161179509+choimik@users.noreply.github.com>
2024-11-06 00:30:38 -0500

    add comments for code

--- Board.java
+++ Board.java
@@ -1,50 +1,40 @@
 //Board File
 
-//Board File
-
 //how to build the board game box
-
 //2 boards letters down the side and numbers across the top (10x10 board)
-
 //5 different ship types
 //Carrier has 5 pegs
-
 //2 colored set of pets
-
-//git functionality test
 
 public class Board{
 	
-	//char[][] board = new char[10][10];
+	//create 2D array to place ships on
+	char[][] boardShips = {{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'}};
 	
-	char[][] board = {{'0','S','0','0','S','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'},{'0','0','0','0','0','0','0','0','0','0'}};
-	
-	/*
-	for(int r = 0; r < 10; r++){
-		for(int c = 0; c < 10; c++){
-			board[r][c] = '0';
-		}
-	}
-	*/
+	//create 2D array to place guesses on
+	char[][] boardGuess = new char[10][10];
 	
 	//constructor
 	public Board(){	
+		
 	}
 	
 	
 	/*
-	 
+	Function checkShip takes two int parameters (row, col) and returns true if there is a ship at that index and false if not
+	Primarily used for boardGuess to check if there is a ship at guess
 	 */
-	public boolean checkShip(int row, int col){
-		if(board[row][col] == 'S'){
+	public boolean checkShip(int row, int col){ //chose to use 'S' to signify a ship
+		if(board[row][col] == 'S'){ //checks if board has a ship ('S') there it returns true
 			return true;
 		}
-		return false;
+		return false; //else return false
 	}
 	
 	
 	/*
-	 
+	function printBoard returns no value and takes no parameters but prints out the current state of the board
+	Can be used for either boardShips or boardGuess (general method)
 	*/
 	public void printBoard(){ //if you name the method toString it will not allow you to run it since it would be overriding toString from String library so I called it printBoard
 		int letter = 65;

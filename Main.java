12b551391918b36b8a92125935368225a3f75787
Mikael Choi <choimik26@7hills.org>
2024-11-12 09:39:16 -0500

    tweak

--- Main.java
+++ Main.java
@@ -7,10 +7,15 @@
 public class Main{
 
   public static void main(String[] args){
+    /*
     Player p1 = new Player();
     p1.getBoardGuesses().printBoard(); //test printBoard
     p1.getBoardShips().printBoard();
     System.out.println(p1.getBoardShips().checkShip(0,1)); //test checkShip
+    */
+    
+    Board b = new Board();
+    b.printBoard();
   }
 
 }

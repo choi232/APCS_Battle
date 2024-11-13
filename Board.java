db0f733df4be6536ae1f68ba14af464ce73c63b2
Mikael Choi <choimik26@7hills.org>
2024-11-13 09:38:42 -0500

    create placeShips method

--- Board.java
+++ Board.java
@@ -8,23 +8,21 @@ Purpose: to build a board for battleship
 Carrier has 5 pegs
 2 colored set of pets
 */
+import java.util.Scanner;
 public class Board{
-	char[][] board = new char[10][10];
+	char[][] ships = new char[10][10];
+	char[][] guess = new char[10][10];
+	
 	//constructor
 	public Board(){	
-		//board with implicit parameters from values
-		for(int r = 0; r < 10; r++){
-			for(int c = 0; c < 10; c++){
-				this.setBoardArray('.', r, c);
-			}
-		}
+		setBoard();
 	}
 	
 	
 	/*
 	function checkShip takes two int parameters (row, col) as well as board object and returns true if there is a ship at that index and false if not
 	 */
-	public boolean checkShip(int row, int col){ //chose to use 'S' to signify a ship
+	public boolean checkShip(char[][] board, int row, int col){ //chose to use 'S' to signify a ship
 		if(board[row][col] == 'S'){ //checks if board has a ship ('S') there it returns true
 			return true;
 		}
@@ -42,18 +40,67 @@ public class Board{
 		for(int r = 0; r < 10; r++, letter++){ //increment both r and letter at same time in for loop
 			System.out.printf("%c ", letter);//print out side header of letters
 			for(int c = 0; c < 10; c++){
-				System.out.print(board[r][c] + " ");//access 2d array and print individual value
+				System.out.print(ships[r][c] + " ");//access 2d array and print individual value
+			}
+			System.out.println(); //print \n to create line break between each row
+		}
+		letter = 65;
+		System.out.println("  0 1 2 3 4 5 6 7 8 9"); //header for columns also used 0-9 instead of 0-10 otherwise there will be an uneven spacing
+		//iterate through 2d array and print
+		for(int r = 0; r < 10; r++, letter++){ //increment both r and letter at same time in for loop
+			System.out.printf("%c ", letter);//print out side header of letters
+			for(int c = 0; c < 10; c++){
+				System.out.print(guess[r][c] + " ");//access 2d array and print individual value
 			}
 			System.out.println(); //print \n to create line break between each row
 		}
 	}
 	
-	public char[][] getBoardArray(){
-		return board;
-	}
-	
-	public void setBoardArray(char marker, int row, int col){
+
+
+	public void setMarker(char[][] board, char marker, int row, int col){
 		board[row][col] = marker;
 	}
 
+	public void setBoard(){
+		for (int row = 0; row < 10; row++){
+			for (int col = 0; col < 10; col++){
+				ships[row][col] = '.';
+				guess[row][col] = '.';
+			}
+		}
+		
+	}
+	//method to place the ships at shiprow(sr) and shipcol(sc)
+	//modify the placeShip method to take in length, startx starty, direction
+	//vertical true places ship vertically downwards from startpoint
+	//vertical false places ship horizontally to the right from the start point
+	public boolean placeShip(int len, int sRowStart, int sColStart, boolean vertical) {
+		//calculate endpoint
+		int sRowEnd;
+		int sColEnd;
+		
+		if(vertical){
+			sRowEnd = sRowStart - len;
+			sColEnd = sColStart - len;
+		}
+		else{
+			sRowEnd = sRowStart + len;
+			sColEnd = sColStart + len;
+		}
+		
+		
+		
+		if(sRowEnd <= 10 && sColEnd <= 10 && vertical){
+			for(int row = sRowStart; row == (sRowEnd+1); row++){
+				ships[row][sColStart] = 's';
+			}
+		}
+		if(sRowEnd <= 10 && sColEnd <= 10 && !(vertical)){
+			for(int col = sColStart; col == (sColEnd+1); col++){
+				ships[sRowStart][sColStart] = 's';
+			}
+		}
+		return true;
+	}
 }
